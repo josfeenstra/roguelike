a83da5e6a1c9a101e05df8fdfd3ea308748e9e94
Jos <me@josfeenstra.nl>
2022-04-29 19:54:05 +0200

    added wasm things :)

--- readme.md
+++ readme.md
@@ -1,2 +1,12 @@
 # Roguelike
 implementing [this](https://bfnightly.bracketproductions.com/rustbook/chapter_0.html) tutorial to learn more about ECS
+
+
+
+# Build 
+
+## Wasm 
+```
+cargo build --release --target wasm32-unknown-unknown
+wasm-bindgen target\wasm32-unknown-unknown\release\roguelike.wasm --out-dir wasm --no-modules --no-typescript
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use rltk::{Rltk, GameState};
+
+struct State {}
+impl GameState for State {
+    fn tick(&mut self, ctx : &mut Rltk) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello Rust World hallo oei this looks nice!");
+        ctx.print(1, 2, "-------------------------------------------");
+    }
+}
+
+fn main() -> rltk::BError {
+
+    use rltk::RltkBuilder;
+    let context = RltkBuilder::simple(50,30).unwrap()
+        .with_title("Roguelike Tutorial")
+        .build()?;
+    let gs = State{ };
+    rltk::main_loop(context, gs)
 }
